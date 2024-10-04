f6e6617210c9ef95b649bcdafe641b45f2957d56
RSYR1906 <rsim9977@gmail.com>
2024-10-04 16:57:05 +0800

    Updates

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount fredAccount = new BankAccount("Fred");
+
+        fredAccount.deposit(1000.0f);
+        fredAccount.withdraw(100.0f);
+
+        FixedDepositAccount juneAccount = new FixedDepositAccount("June", 100.0f, 5.0f);
+
+        juneAccount.displayAccDetails();
+    }
+}
